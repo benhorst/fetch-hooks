ce346f98605281de907668dcdba803c6197e1365
benhorst <benjamin.horst@accolade.com>
2019-11-20 11:54:25 -0800

    Cleanup on components, allow for input on the url fetch example

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
-const { useReducer, useEffect } = React;
+const { useReducer, useEffect, useState } = React;
+
+// NOTE TO FRIENDS: sorry about the semi-inconsistent semi-colons.
 
 interface FetchRequest {
   url: string
@@ -25,6 +27,9 @@ interface Action {
   type: string
   payload: any
 }
+interface UseFetchOptions {
+  parser: (input:string) => any
+}
 
 const fetchReducer = (state: FetchResource<any>, action: Action) => {
   const { type, payload } = action;
@@ -32,13 +37,14 @@ const fetchReducer = (state: FetchResource<any>, action: Action) => {
     case 'init':
       return PRISTINE_FETCH_STATE;
     case 'start-load':
+      // on initial state, the status=0 because we don't know yet, but the previous one is gone
       return {
         ...state,
         loading: true,
         request: {
           status: 0,
           url: payload.url,
-          promise: payload.fetcher,
+          promise: payload.promise,
         },
       };
     case 'end-load':
@@ -61,82 +67,93 @@ const fetchReducer = (state: FetchResource<any>, action: Action) => {
     default:
       throw new Error();
   }
-  
 }
 
 const DEFAULT_FETCH_OPTIONS = {
     method: 'get',
     credentials: 'include',
 }
-export const useFetch = (url:string, fetchOptions: any = DEFAULT_FETCH_OPTIONS) => {
+const jsonString = (input:string) => JSON.parse(input);
+export const useFetch = (
+  url:string,
+  fetchOptions: any = DEFAULT_FETCH_OPTIONS,
+  // by default, we'll parse the output as json.
+  options: UseFetchOptions = { parser: jsonString }
+) => {
     const [state, dispatch] = useReducer(fetchReducer, PRISTINE_FETCH_STATE);
     useEffect(() => {
         let error:any = null;
         let status:number = 0;
-        const request = fetch(url, fetchOptions)
-          .then(response => {
-            if (!response.ok) {
-              error = true;
-            }
-            status = response.status;
-            return response.text();
-          }, (err) => {
-            // fetch failed completely
-            dispatch({
-              type: 'end-load',
-              payload: {
-                error: { message: 'failed to fetch.', inner: err },
-              }
-            });
-          }).then(text => {
-            let json:any;
-            try {
-              if (text) {
-                json = JSON.parse(text);
-              }
-              setTimeout(
-                () => {
-                  dispatch({
-                    type: 'end-load',
-                    payload: {
-                      request: {
-                        status,
-                      },
-                      loaded: true,
-                      loading: false,
-                      error,
-                      data: json || text
-                    }
-                  });
-                },
-                3000
-              )
-              
-            } catch (ex) {
-              dispatch({
-                type: 'end-load',
-                payload: {
-                  error: { message: 'failed to parse the response as json.', inner: ex },
-                }
-              });
-            }
 
-          }, (err) => {
-            // turning the response into text failed.
-            dispatch({
-              type: 'end-load',
-              payload: {
-                error: { message: 'failed to fetch.', inner: err },
-              }
-            });
-          })
+        const request = fetch(url, fetchOptions)
+        // first stage of response, should be 'ok' or not
+        .then(response => {
+          if (!response.ok) {
+            error = true;
+          }
+          status = response.status;
+          return response.text();
+        }, (err) => {
+          // fetch failed completely
           dispatch({
-            type: 'start-load',
+            type: 'end-load',
             payload: {
-              fetcher: request,
-              url,
+              error: { message: 'failed to fetch.', inner: err },
             }
           });
+        })
+        // next we enter the parsing stage
+        .then(body => {
+          let json:any;
+          try {
+            if (body) {
+              json = options.parser(body);
+            }
+            // we are setting a timeout just so that load is more clear
+            // TODO: remove this synthetic delay.
+            setTimeout(
+              () => {
+                dispatch({
+                  type: 'end-load',
+                  payload: {
+                    request: {
+                      status,
+                    },
+                    loaded: true,
+                    loading: false,
+                    error,
+                    data: json || body
+                  }
+                });
+              },
+              3000
+            );
+          } catch (ex) {
+            // the parser failed to get what it wanted.
+            dispatch({
+              type: 'end-load',
+              payload: {
+                error: { message: 'Failed to parse the body of the response.', inner: { exception: ex, body } },
+              }
+            });
+          }
+
+        }, (err) => {
+          // turning the response into text failed.
+          dispatch({
+            type: 'end-load',
+            payload: {
+              error: { message: 'failed to fetch.', inner: err },
+            }
+          });
+        });
+      dispatch({
+        type: 'start-load',
+        payload: {
+          promise: request,
+          url,
+        }
+      });
     }, [url]);
 
     return [state];
@@ -175,15 +192,19 @@ export const StatusWrapper = (props:StatusWrapperProps) => {
 };
 
 export const ExampleComponent = () => {
-  const [data] = useFetch('https://my-json-server.typicode.com/typicode/demo/posts', { method: 'get', headers: {} } );
+  const [url, setUrl] = useState('https://my-json-server.typicode.com/typicode/demo/posts');
+  const [data] = useFetch(url, { method: 'get', headers: {} } );
   return (
-    <StatusWrapper fetched={data}>
-      <pre>
-        {
-          JSON.stringify(data, null, 2)
-        }
-      </pre>
-    </StatusWrapper>
+    <div>
+      <input style={ { width: '100%' } } type="text" value={ url } onChange={ ev => setUrl(ev.target.value) } />
+      <StatusWrapper fetched={data}>
+        <pre>
+          {
+            JSON.stringify(data, null, 2)
+          }
+        </pre>
+      </StatusWrapper>
+    </div>
   );
 }
 
