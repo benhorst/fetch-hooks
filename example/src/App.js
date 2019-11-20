77c0bcccb99afd42ac81be64a7e18b2d9e601cf2
benhorst <benjamin.horst@accolade.com>
2019-11-20 11:33:19 -0800

    Fix the example to import correctly and dedup react

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'fetch-hooks'
+import { ExampleComponent } from 'fetch-hooks'
 
 export default class App extends Component {
   render () {
